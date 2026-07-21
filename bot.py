fa479c901a28b60c8975466427c0a325c18fa728
mikacswinpay-oss <mikacswinpay@gmail.com>
2026-07-21 17:29:58 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 from telegram import Update, ReplyKeyboardMarkup
 from telegram.ext import Application, CommandHandler, ContextTypes
 
-TOKEN = "8771022249:AAEniW8DJDVJe41AxD6JMJqtZav1bgeos6A"
+TOKEN = "8771022249:AAHF3MQkMrwRZOMe545Mq8xcwggk087Mw1s"
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     keyboard = [

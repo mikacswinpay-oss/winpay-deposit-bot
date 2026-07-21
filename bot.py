28ab31a704b8d0419f1984d6cbdd42fbe9b354aa
mikacswinpay-oss <mikacswinpay@gmail.com>
2026-07-21 17:28:26 +0530

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,25 @@
+from telegram import Update, ReplyKeyboardMarkup
+from telegram.ext import Application, CommandHandler, ContextTypes
+
+TOKEN = "8771022249:AAEniW8DJDVJe41AxD6JMJqtZav1bgeos6A"
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    keyboard = [
+        ["💰 Deposit (UPI)", "🎁 Bonus Offers"],
+        ["👑 Premium", "📄 How to Deposit"],
+        ["🎧 Customer Support"]
+    ]
+
+    await update.message.reply_text(
+        "👋 Welcome to WinPay!\n\n"
+        "💰 Offline UPI Deposit Available\n"
+        "🎁 Exclusive Bonus\n"
+        "🛡️ Safe & Secure\n"
+        "⚡ Fast Approval",
+        reply_markup=ReplyKeyboardMarkup(keyboard, resize_keyboard=True)
+    )
+
+app = Application.builder().token(TOKEN).build()
+app.add_handler(CommandHandler("start", start))
+
+app.run_polling()
